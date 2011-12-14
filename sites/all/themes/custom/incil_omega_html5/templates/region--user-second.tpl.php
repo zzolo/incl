b2a8af289537b07ac5003191a2a68cf2ab9197a3
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2011-12-13 19:02:49 -0800

    More updates to theme.

---
+++ sites/all/themes/custom/incil_omega_html5/templates/region--user-second.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Altered to add social network links
+ */
+?>
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <div class="social-container">
+      <a href="" class="social-facebook"></a>
+      <a href="" class="social-twitter"></a>
+      <a href="" class="social-google"></a>
+    </div>
+    
+    <?php print $content; ?>
+  </div>
+</div>
