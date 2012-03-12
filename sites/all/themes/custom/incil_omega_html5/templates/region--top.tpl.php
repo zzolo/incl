2ffd05ff92ce7cce49d8d5bfa43c31e1f7fa26ab
Pete Fecteau <pete@buttonpresser.com>
2012-03-12 14:00:42 -0700

    added smaller call-to-actions to the home page, under the nav.

---
+++ sites/all/themes/custom/incil_omega_html5/templates/region--top.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Altered to add Call-to-Actions
+ */
+?>
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <div class="call-to-actions">
+      <a href="https://infoinnovation.org/explore" class="cta"><img src="https://infoinnovation.org/sites/all/themes/custom/incil_omega_html5/images/button-explore.png"></a>
+      <a href="https://infoinnovation.org/implement" class="cta"><img src="https://infoinnovation.org/sites/all/themes/custom/incil_omega_html5/images/button-implement.png"></a>
+      <a href="https://infoinnovation.org/interact" class="cta" style="margin-right:0px;"><img src="https://infoinnovation.org/sites/all/themes/custom/incil_omega_html5/images/button-interact.png"></a>
+    </div>
+    
+    <?php print $content; ?>
+  </div>
+</div>
