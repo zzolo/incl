f7f3a2bc154e1486d3d362ed75567108cc94005c
Pete Fecteau <pete@buttonpresser.com>
2012-03-13 09:55:13 -0700

    fixing the linking on the logo region

--- sites/all/themes/custom/incil_omega_html5/templates/region--branding-second.tpl.php
+++ sites/all/themes/custom/incil_omega_html5/templates/region--branding-second.tpl.php
@@ -6,12 +6,11 @@
 ?>
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
+    <?php print $content; ?>
     <div class="social-container">
       <a href="http://www.facebook.com/internews.innovation" class="social-facebook"></a>
       <a href="http://twitter.com/info_innovation" class="social-twitter"></a>
       <!-- <a href="" class="social-google"></a> -->
     </div>
-    
-    <?php print $content; ?>
   </div>
 </div>
